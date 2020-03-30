014907003485388d13f7f9d1094da12a64bf37be
yakumo17s <wo409697458@gmail.com>
2020-03-30 17:21:36 +0800

    初始化 添加获取东方project api的桌面背景

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace TouhouDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AutoScroll = true;
+            this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(263, 315);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "东方project";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 175);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(146, 216);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(38, 21);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F);
+            this.label2.Location = new System.Drawing.Point(190, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "秒";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "随机换图";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "打开文件夹";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(32, 214);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "定时修改";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "修改存储地址";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(271, 341);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(263, 315);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "konachan";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 319);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(271, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(271, 341);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "桌面壁纸";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,251 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Console;
+
+namespace TouhouDesktop
+{
+    public partial class Form1 : Form
+    {
+        private const string paulzzhUrl = "https://img.paulzzh.tech/touhou/random?site=all";
+
+        private string directory = @"D:\img\";
+        private System.Timers.Timer timer;
+        const int SPI_GETDESKWALLPAPER = 0x0073;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WriteLine(this.directory);
+            textBox1.Text = this.directory;
+
+        }
+
+        private void GetCurrentWallPaper()
+        {
+            StringBuilder wallPaperPath = new StringBuilder("a", 50);
+            //string a = "                                                                                    ";
+            string[] b = Registry.CurrentUser.GetSubKeyNames();
+            foreach (string c in b)
+            {
+                WriteLine(c);
+
+            }
+            //Registry.CurrentUser.OpenSubKey("Control Panel", new RegistryKeyPermissionCheck()).CreateSubKey("C D");
+            //.OpenSubKey("Desktop").GetValue("WallPaper"));
+            //myRegKey = myRegKey.OpenSubKey("Desktop");
+            //Registry.CurrentUser.CreateSubKey(@"A B");
+            //Registry.CurrentUser.OpenSubKey("A B", true).CreateSubKey("C D");
+
+            //object a = Registry.CurrentUser.GetValue("Control Panel/Desktop/WallPaper");
+
+            //if (Convert.ToBoolean(SystemParametersInfo(SPI_GETDESKWALLPAPER, 50, a, 0)))
+            //{
+            //    a.Replace(" ", "");a.Trim();
+            //    WriteLine($"|{a.Substring(0)}|");
+
+            //    WriteLine($"|{a}|");
+            //    tabPage1.BackgroundImage = Image.FromFile(a);
+            //}
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            await GetPicture();
+
+        }
+
+
+
+        private async Task GetPicture()
+        {
+
+            using (var client = new HttpClient())
+            {
+
+                try
+                {
+                    // 下载图片
+                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+                    toolStripStatusLabel1.Text = "正在下载";
+
+                    HttpResponseMessage rp = await client.GetAsync(paulzzhUrl);
+
+                    rp.EnsureSuccessStatusCode();
+
+                    string rp_body = await rp.Content.ReadAsStringAsync();
+
+                    toolStripStatusLabel1.Text = $"下载完成{rp_body.Length / 1024 } KB";
+
+                    // 读取图片资源
+                    System.Drawing.Image downImage = System.Drawing.Image.FromStream(await rp.Content.ReadAsStreamAsync());
+
+                    // 保存图片 
+                    string deerory = string.Format(this.directory + @"{0}\", DateTime.Now.ToString("yyyy-MM-dd"));
+
+                    string fileName = string.Format("{0}.png", DateTime.Now.ToString("HHmmssffff"));
+
+
+                    if (!System.IO.Directory.Exists(deerory))
+                    {
+                        System.IO.Directory.CreateDirectory(deerory);
+                    }
+
+                    downImage.Save(deerory + fileName);
+
+                    // 设置为tab背景
+                    try
+                    {
+                        tabPage1.BackgroundImage = Image.FromFile(deerory + fileName);
+
+                    }
+                    catch (Exception err)
+                    {
+                        WriteLine(err);
+                    }
+
+                    downImage.Dispose();
+
+
+                    SystemParametersInfo(20, 1, deerory + fileName, 1);
+
+                    WriteLine("获取随机图片结束");
+
+                    return;
+
+                }
+                catch (Exception err)
+                {
+                    toolStripStatusLabel1.Text = $"下载失败：{err}";
+                    WriteLine(err);
+                    return;
+                }
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        private void menuItemCenter_Click(object sender, System.EventArgs e)
+        {
+            //设置墙纸显示方式
+            RegistryKey myRegKey = Registry.CurrentUser.OpenSubKey("Control Panel/desktop", true);
+            //赋值
+            //注意：在把数值型的数据赋到注册表里面的时候，
+            //如果不加引号，则该键值会成为“REG_DWORD”型；
+            //如果加上引号，则该键值会成为“REG_SZ”型。
+            myRegKey.SetValue("TileWallpaper", "0");
+            myRegKey.SetValue("WallpaperStyle", "0");
+
+            //关闭该项,并将改动保存到磁盘
+            myRegKey.Close();
+
+            //设置墙纸
+            /* Bitmap bmpWallpaper = (Bitmap)pictureBox1.Image;
+             bmpWallpaper.Save("resource.bmp", ImageFormat.Bmp); *///图片保存路径为相对路径，保存在程序的目录下
+            string strSavePath = Application.StartupPath + "/resource.bmp";
+            SystemParametersInfo(20, 1, strSavePath, 1);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+            // 检查文件夹是否存在
+            if (!System.IO.Directory.Exists(this.directory))
+            {
+                System.IO.Directory.CreateDirectory(this.directory);
+            }
+
+            System.Diagnostics.Process.Start(this.directory);
+        }
+
+
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            this.directory = textBox1.Text;
+            toolStripStatusLabel1.Text = $"已修改图片存储目录";
+        }
+
+
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+
+            // 停止定时器
+            if (!(timer is null) && timer.Enabled)
+            {
+                timer.Stop();
+                timer.Dispose();
+                toolStripStatusLabel1.Text = "已停止定时修改";
+                button4.Text = "启动定时修改";
+                return;
+            }
+
+            // 启动定时器
+            bool rp = double.TryParse(textBox2.Text, out double time);
+
+            WriteLine($"{rp}:{time}");
+
+            if (rp)
+            {
+
+
+                timer = new System.Timers.Timer
+                {
+                    // 秒->毫秒
+                    Interval = time * 1000,
+                    //timer.Enabled = true;
+                    AutoReset = false//设置是执行一次（false）还是一直执行(true)；  
+                };
+                timer.Start();
+
+                timer.Elapsed += (o, a) =>
+                {
+
+                    this.Invoke(new TimerGetPictureDelegate(TimerGetPicture));
+
+                };
+
+                // 文本修改
+                toolStripStatusLabel1.Text = "启动定时修改";
+                button4.Text = "停止定时修改";
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "时间设置错误";
+            }
+        }
+
+        public delegate void TimerGetPictureDelegate();
+
+        private async void TimerGetPicture()
+        {
+
+            await GetPicture();
+            // 完成任务后，启动下一次定时
+            timer.Start();
+
+            // toolStripStatusLabel1.Text = $"下次修改时间：{DateTime.Now + TimeSpan.FromSeconds(timer_interval)}";
+
+
+        }
+    }
+}
